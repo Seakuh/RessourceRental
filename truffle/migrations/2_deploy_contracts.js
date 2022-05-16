c784dd9fe358c98e94d4942b1b45347adeebaa7f
DizzleReply <D.grimm@reply.de>
2022-05-16 09:34:44 +0200

    cleanup

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,5 +1,16 @@
+<<<<<<< Updated upstream
 const ResourceRental = artifacts.require("ResourceRental");
 
 module.exports = function (deployer) {
   deployer.deploy(ResourceRental);
+=======
+const ConvertLib = artifacts.require("ConvertLib");
+const MetaCoin = artifacts.require("MetaCoin");
+const Resource_Rental_Admin = artifacts.require("Resource_Rental_Admin.sol");
+
+module.exports = function (deployer) {
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(MetaCoin);
+>>>>>>> Stashed changes
 };
